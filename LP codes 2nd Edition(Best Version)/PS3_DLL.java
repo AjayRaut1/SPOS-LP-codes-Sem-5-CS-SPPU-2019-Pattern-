30bdec41b4dc3fc628d5435e66681d451e45101d
Ajay Raut <ajauraut2002@gmail.com>
2022-11-24 02:17:56 +0530

    Update PS3_DLL.java

---
+++ LP codes 2nd Edition(Best Version)/PS3_DLL.java
@@ -0,0 +1,42 @@
+//Step 1 : After creating Calculator dll , paste below code inside CalculatorDLL.h file
+
+#ifndef  _CALCULATORDLL_h_
+#define  _CALCULATORDLL_h_
+#ifdef CALCULATORDLL_EXPORTS
+#define CALCULATORDLL_API __declspec(dllexport)
+ 
+#else
+#define CALCULATORDLL_API __declspec(dllimport)
+
+#endif
+CALCULATORDLL_API int Addition(int x,int y);
+#endif
+
+//Step 2 : After creating CalculatorDLL.h file create CalculatorDLL.c file and paste below code
+
+#ifndef _CALCULATORDLL_c_
+#define _CALCULATORDLL_c_
+ 
+#include "CalculatorDLL.h" 
+
+int Addition(int x,int y){
+	int z;
+	z=x+y;
+	return z;
+}
+#endif
+
+//Step 3 : After creating Calculator app paste the below code inside CalculatorAPP.c file
+
+#include<stdio.h>
+#include"C:\Users\ajaur\OneDrive\Documents\Visual Studio 2012\Projects\CalculatorDLL\CalculatorDLL\CalculatorDLL.h"
+
+int main()
+{
+	int a;
+	a = Addition(10,5);
+	printf("Addition=%d\n",a);
+	return 0;
+}
+
+
